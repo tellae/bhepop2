4570aab5fb10bfaa7a8812830f00171d09bfa7a8
Vincent Leblond <vincent.leblond@gmail.com>
2022-05-04 13:17:33 +0200

    feat: read filosofi data directly from raw excel files

--- src/nantes_application.py
+++ src/nantes_application.py
@@ -11,8 +11,17 @@ DECILE_10 = 1.5
 
 PATH_PROCESSED = "../data/processed/"
 PATH_INPUTS = "../data/inputs/"
+PATH_RAW = "../data/raw/"
 SYNTHETIC_POP = "nantes_synth_pop.csv"
 FILOSI_DECILES = "deciles_filosofi.feather"
+FILOSOFI = "indic-struct-distrib-revenu-2015-COMMUNES/"
+CODE_INSEE = "44109"
+
+# Set display options
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 100)
+pd.set_option("display.width", 1000)
+pd.set_option("mode.chained_assignment", None)
 
 # %%
 # script 1
@@ -42,6 +51,75 @@ group = group[["ownership", "age", "size", "family_comp", "probability"]]
 # %%
 # script 2
 # TODO read data directly from xlsx INSEE data
+
+FILOSOFI_MODALITIES = [
+    {"name": "1 person", "sheet": "TAILLEM_1", "col_pattern": "TME1"},
+    {"name": "2 persons", "sheet": "TAILLEM_2", "col_pattern": "TME2"},
+    {"name": "3 persons", "sheet": "TAILLEM_3", "col_pattern": "TME3"},
+    {"name": "4 persons", "sheet": "TAILLEM_4", "col_pattern": "TME4"},
+    {"name": "5 persons or more", "sheet": "TAILLEM_5", "col_pattern": "TME5"},
+    {"name": "Single man", "sheet": "TYPMENR_1", "col_pattern": "TYM1"},
+    {"name": "Single woman", "sheet": "TYPMENR_2", "col_pattern": "TYM2"},
+    {"name": "Couple without children", "sheet": "TYPMENR_3", "col_pattern": "TYM3"},
+    {"name": "Couple with children", "sheet": "TYPMENR_4", "col_pattern": "TYM4"},
+    {"name": "Single parent family", "sheet": "TYPMENR_5", "col_pattern": "TYM5"},
+    {"name": "Complex households", "sheet": "TYPMENR_6", "col_pattern": "TYM6"},
+    {"name": "0_29", "sheet": "TRAGERF_1", "col_pattern": "AGE1"},
+    {"name": "30_39", "sheet": "TRAGERF_2", "col_pattern": "AGE2"},
+    {"name": "40_49", "sheet": "TRAGERF_3", "col_pattern": "AGE3"},
+    {"name": "50_59", "sheet": "TRAGERF_4", "col_pattern": "AGE4"},
+    {"name": "60_74", "sheet": "TRAGERF_5", "col_pattern": "AGE5"},
+    {"name": "75_or_more", "sheet": "TRAGERF_6", "col_pattern": "AGE6"},
+    {"name": "Owner", "sheet": "OCCTYPR_1", "col_pattern": "TOL1"},
+    {"name": "Tenant", "sheet": "OCCTYPR_2", "col_pattern": "TOL2"},
+]
+
+filosofi = pd.DataFrame()
+
+for modality in FILOSOFI_MODALITIES:
+    SHEET = modality["sheet"]
+    print(SHEET)
+    COL_PATTERN = modality["col_pattern"]
+    tmp = pd.read_excel(
+        PATH_RAW + FILOSOFI + "FILO_DISP_COM.xls",
+        sheet_name=SHEET,
+        skiprows=5,
+    ).query("CODGEO=='" + CODE_INSEE + "'")
+    tmp["modality"] = modality["name"]
+    tmp = tmp.rename(
+        columns={
+            COL_PATTERN + "D115": "D1",
+            COL_PATTERN + "D215": "D2",
+            COL_PATTERN + "D315": "D3",
+            COL_PATTERN + "D415": "D4",
+            COL_PATTERN + "Q215": "D5",
+            COL_PATTERN + "D615": "D6",
+            COL_PATTERN + "D715": "D7",
+            COL_PATTERN + "D815": "D8",
+            COL_PATTERN + "D915": "D9",
+        }
+    )
+    tmp = tmp[
+        [
+            "CODGEO",
+            "LIBGEO",
+            "modality",
+            "D1",
+            "D2",
+            "D3",
+            "D4",
+            "D5",
+            "D6",
+            "D7",
+            "D8",
+            "D9",
+        ]
+    ]
+    filosofi = pd.concat([filosofi, tmp])
+
+
+# %%
+
 decile_total = pd.read_feather(PATH_PROCESSED + FILOSI_DECILES)
 decile_total["D0"] = DECILE_0
 decile_total["D10"] = decile_total["D9"] * DECILE_10
