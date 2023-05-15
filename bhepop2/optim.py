f9b5e69e62e7cfa217a75bd29cb7e7f85b81396d
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2023-05-15 15:50:59 +0200

    renames and fixes

--- bhepop2/optim.py
+++ bhepop2/optim.py
@@ -1,24 +1,18 @@
 
 import numpy as np
-import time
 from numpy.linalg import norm
 from .utils import log
 import logging as lg
 
 
-def minxent_gradient(q: np.ndarray, G: np.ndarray, eta: np.ndarray, lambda_: np.ndarray, maxiter: int):
-    startTime = time.time()
+def minxent_gradient(q: np.ndarray, matrix: np.ndarray, eta: np.ndarray, lambda_: np.ndarray, maxiter: int):
     q = q.astype(float)
-    G = G.astype(float)
+    matrix = matrix.astype(float)
     eta = eta.astype(float)
     lambda_ = lambda_.astype(float)
-    G_full = G.copy()
-    eta_full = eta.copy()
-    G = G_full[1:, :]
-    eta = eta_full[:, 1:]
-    dimG = G.shape
-    ncons = dimG[0]  # POV number of constrains wihout the natural constraints
-    nproba = dimG[1]  # POV number of probabilites to find
+    matrix = matrix.copy()[1:, :]
+    eta = eta.copy()[:, 1:]
+    ncons, nproba = matrix.shape
     q = q.reshape(nproba, 1)
     lambda_ = lambda_.reshape(ncons, 1)
     eta = eta.reshape(ncons, 1)
@@ -29,39 +23,29 @@ def minxent_gradient(q: np.ndarray, G: np.ndarray, eta: np.ndarray, lambda_: np.
     common_ratio_descending = 0.5
     common_ratio_ascending = 2.0
 
-    lambda_old = None
-
     dist = 1
-
     while iter_general <= maxiter and dist >= 1e-08:
         iter_general += 1
-        log("Mixent gradient iteration : " + str(iter_general), lg.DEBUG)
+        log("Minxent gradient iteration : " + str(iter_general), lg.DEBUG)
 
         # lambda0 = np.log(np.sum(q * np.exp(-lambda_old.dot(G[1:, :])))) Exp_neg_Gt_lambdaPOV
-        Exp_neg_Gt_lambda = np.exp(-G.T.dot(lambda_))  # POV exp(-Gt.lambda)
+        Exp_neg_Gt_lambda = np.exp(-matrix.T.dot(lambda_))  # POV exp(-Gt.lambda)
         lambda0 = np.log(q.T.dot(Exp_neg_Gt_lambda))
         pk = (q * Exp_neg_Gt_lambda) / (q.T.dot(Exp_neg_Gt_lambda))
         level_objective = lambda0 + np.sum(lambda_ * eta)
-        f_old = eta - G.dot(pk)
-
-        # f_old = fk(q=q, G=G, eta=eta, lambda_=lambda_old)
-
-        # dev_ent = np.dot(q * np.exp(-lambda0) * np.exp(-lambda_old.dot(G[1:, :])), G)
-        # pg = q * np.exp(-lambda0) * np.exp(-lambda_old.dot(G[1:, :]))
+        f_old = eta - matrix.dot(pk)
 
         alpha_ascent = 1.0
         alpha_descent = 1.0
         alpha = 1.0
         lambda_old = lambda_.copy()
         lambda_ = lambda_.copy()
-        lambda_new = lambda_.copy()
-        level_objective_new = level_objective
         did_ascent = False
         did_descent = False
 
         while not (did_ascent and did_descent):
             lambda_new = lambda_old - alpha * f_old
-            lambda0 = np.log(q.T.dot(np.exp(-G.T.dot(lambda_new))))
+            lambda0 = np.log(q.T.dot(np.exp(-matrix.T.dot(lambda_new))))
             level_objective_new = lambda0 + np.sum(lambda_new * eta)
 
             # level_objective_new = objective(q=q, G=G, eta=eta, lambda_=lambda_new)
@@ -84,15 +68,13 @@ def minxent_gradient(q: np.ndarray, G: np.ndarray, eta: np.ndarray, lambda_: np.
 
         dist = norm(lambda_ - lambda_old)
 
-    endTime = time.time()
-    duration_iter = endTime - startTime
-    # lambda0 = np.log(np.sum(q * np.exp(-lambda_.dot(G[1:, :]))))
-
-    Exp_neg_Gt_lambda = np.exp(-G.T.dot(lambda_))  # POV exp(-Gt.lambda)
+    Exp_neg_Gt_lambda = np.exp(-matrix.T.dot(lambda_))  # POV exp(-Gt.lambda)
     lambda0 = np.log(q.T.dot(Exp_neg_Gt_lambda))
     pk = (q * Exp_neg_Gt_lambda) / (q.T.dot(Exp_neg_Gt_lambda))
+
+    # remove ?
     # pi_solve = q * np.exp(-lambda0) * np.exp(-lambda_.dot(G[1:, :]))
     Lagrangians.append([lambda0, lambda_.tolist()])
-    test_pierreolivier = G.dot(pk) - eta
+    test_pierreolivier = matrix.dot(pk) - eta
 
     return pk.tolist(), lambda_
