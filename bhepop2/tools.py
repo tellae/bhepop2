546f3da46523cdd6c1267d01389adbb74344776d
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2023-05-17 14:41:08 +0200

    new function for adding household type to population

--- bhepop2/tools.py
+++ bhepop2/tools.py
@@ -346,7 +346,117 @@ def add_household_size_attribute(population: pd.DataFrame, values_map:callable=l
 
     return population
 
+def add_household_type_attribute(population, person_id:str= "person_id", household_id:str= "household_id", column_name="family_comp"):
+    """
+    Add a type attribute to the given synthetic population.
 
+    The following attributes are needed on the population:
+        - household_size: number of person's in the household
+        - sex: person's sex
+        - age: person's age
+        - couple: boolean indicating if the person is in couple in the household
+
+    The household type can take the following values:
+        - Single_man
+        - Single_wom
+        - Couple_without_child
+        - Couple_with_child
+        - Single_parent
+        - complex_hh
+
+    Even though we add the attribute at a person's level,
+    its value is related to the household (we add household type).
+
+    :param population: synthetic population df
+    :param person_id: name of the column containing persons' ids
+    :param household_id: name of the column containing households' ids
+    :param column_name: name of the added column
+
+    :return: population with new 'column_name' column containing household size
+    """
+
+    # make copy of original population
+    population = population.copy()
+
+    # get single men and women
+    df_households_info = population.groupby([household_id], as_index=False).agg(
+        {"household_size": "first", "sex": "first"}
+    )
+    df_households_single_man = df_households_info.query(
+        "household_size==1 and sex=='male'"
+    ).reset_index()
+    df_households_single_man[column_name] = "Single_man"
+    df_households_single_woman = df_households_info.query(
+        "household_size==1 and sex=='female'"
+    ).reset_index()
+    df_households_single_woman[column_name] = "Single_wom"
+
+    # get couple without child
+    # 2 persons and with couple=True
+    df_households_couple = (
+        population.query("couple==True")
+        .groupby([household_id], as_index=False)
+        .agg({"couple": "count", "household_size": "first"})
+        .query("couple==2")
+    )
+    df_households_couple_without_child = df_households_couple.query(
+        "couple==2 and household_size==2"
+    ).reset_index()
+    df_households_couple_without_child[column_name] = "Couple_without_child"
+
+    # get couple with child
+    # 3 persons or more, 2 persons in couple, and max age of person non couple < 25
+    df_households_child = (
+        population.query("age<25 and couple==False")
+        .groupby([household_id], as_index=False)
+        .agg({person_id: "count"})
+        .rename(columns={person_id: "child_count"})
+    )
+    df_households_couple_with_child = (
+        df_households_couple.merge(df_households_child, how="left", on=household_id)
+        .query("household_size==child_count+2")
+        .reset_index()
+    )
+    df_households_couple_with_child[column_name] = "Couple_with_child"
+
+    # get single parent
+    # 2 persons or more, no one in couple, oldest person = [25-60[, and others age < 25
+    df_households_no_couple = population.query(
+        "couple==False and household_size>=2"
+    ).sort_values([household_id, "age"], ascending=[True, False])
+
+    df_households_no_couple_oldest = (
+        df_households_no_couple.groupby([household_id], as_index=False)
+        .agg({"age": "first", person_id: "first"})
+        .rename(columns={"age": "oldest_age", person_id: "oldest_person_id"})
+    )
+
+    df_households_no_couple_second_oldest = (
+        df_households_no_couple.merge(df_households_no_couple_oldest, how="left")
+        .query(f"{person_id}!=oldest_person_id")
+        .sort_values([household_id, "age"], ascending=[True, False])
+        .groupby([household_id], as_index=False)
+        .agg({"age": "first"})
+        .rename(columns={"age": "second_oldest_age"})
+    )
+
+    df_households_single_parent = df_households_no_couple_oldest.merge(
+        df_households_no_couple_second_oldest, how="left", on=household_id
+    )
+    df_households_single_parent = df_households_single_parent.query(
+        "oldest_age>=25 and oldest_age<60 and second_oldest_age<25"
+    ).reset_index()
+    df_households_single_parent[column_name] = "Single_parent"
+
+    # combine type
+    combined = pd.concat([df_households_single_man, df_households_single_woman, df_households_couple_without_child, df_households_couple_with_child, df_households_single_parent])[[household_id, column_name]]
+
+    assert combined[household_id].is_unique
+
+    population = population.merge(combined,how="left", on=household_id)
+    population[column_name].fillna("complex_hh", inplace=True)
+
+    return population
 
 def compute_distribution(df: pd.DataFrame) -> pd.DataFrame:
     """
