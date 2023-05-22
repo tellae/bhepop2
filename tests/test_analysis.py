1527fe05ac6e310b3f72382d474378c57507fdaf
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2023-05-22 17:40:31 +0200

    black style

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -8,7 +8,12 @@ def test_analyse_enriched_population(filosofi_distributions_nantes, test_modalit
     populations = {"base": pop}
 
     analyse_enriched_populations(
-        populations, filosofi_distributions_nantes, "Filosofi", test_modalities, tmp_dir, plots=False
+        populations,
+        filosofi_distributions_nantes,
+        "Filosofi",
+        test_modalities,
+        tmp_dir,
+        plots=False,
     )
 
 
